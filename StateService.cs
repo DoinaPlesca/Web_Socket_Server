882817f1ff5aedfccca038a9b1f73e0c49d99465
Doina Plesca <doinitzaplesca@yahoo.com>
2024-02-26 09:59:57 +0100

    data structure for charRooms, event handler,for room brodcasting

---
+++ StateService.cs
@@ -0,0 +1,47 @@
+﻿using Fleck;
+
+namespace websocket;
+//hold webSocketConnection and do what is asociate with conn
+public class WebSocketMetaData(IWebSocketConnection connection)
+{
+    public IWebSocketConnection Connection { get; set; } = connection;
+    public string Username { get; set; } //
+}
+
+public static class StateService
+{
+    //single source connection
+    public static Dictionary<Guid, WebSocketMetaData> Connections = new();
+
+    //Guid...refer just to 1 connection,inside of the tum 'int'
+    // int = number of room
+    public static Dictionary<int, HashSet<Guid>> Rooms = new();
+
+    public static bool AddConnection(IWebSocketConnection ws)
+    {
+        return Connections.TryAdd(ws.ConnectionInfo.Id,
+            new WebSocketMetaData(ws));
+    }
+
+    //'room' is the key of the Room
+    public static bool AddToRoom(IWebSocketConnection ws, int room)
+    {
+        if (!Rooms.ContainsKey(room))
+            Rooms.Add(room, new HashSet<Guid>());
+
+        return Rooms[room].Add(ws.ConnectionInfo.Id);
+    }
+
+    //we go over each GuidID in the room and broadcast to the webSocket
+    public static void BroadcastToRoom(int room, string message)
+    {
+        if (Rooms.TryGetValue(room, out var guids))
+        {
+            foreach (var guid in guids)
+            {
+                if (Connections.TryGetValue(guid, out var webSocketConnection))
+                    webSocketConnection.Connection.Send(message);
+            }
+        }
+    }
+}
---
+++ frontend/src/app/handler.service.ts
@@ -0,0 +1,23 @@
+﻿import { Injectable } from '@angular/core';
+import { BaseDto, ServerEchosClientDto } from '../BaseDto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageHandlerService {
+
+  constructor() { }
+
+  handleIncomingMessage(message: BaseDto<any>, messages: string[]) {
+    switch (message.eventType) {
+      case 'ServerEchosClientDto':
+        this.handleServerEchosClientDto(message as ServerEchosClientDto, messages);
+        break;
+      // Add cases for other message types as needed
+    }
+  }
+
+  handleServerEchosClientDto(dto: ServerEchosClientDto, messages: string[]) {
+    messages.push(dto.echoValue!);
+  }
+}
